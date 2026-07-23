55a031123b3ff21bc30e0dae14459e1cc91825b2
sindhukanakapudi <sindhukanakapudi@gmail.com>
2026-07-23 09:35:45 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+    
